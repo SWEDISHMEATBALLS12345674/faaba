6eefbdcf333e2f4b91959e80bc8d5f35fcb6ac00
SWEDISHMEATBALLS12345674 <72586827+SWEDISHMEATBALLS12345674@users.noreply.github.com>
2024-01-07 17:30:15 -0500

    no need for silly comments

--- src/client/java/liraaa/LiraaaClient.java
+++ src/client/java/liraaa/LiraaaClient.java
@@ -6,6 +6,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class LiraaaClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		
 	}
 }
